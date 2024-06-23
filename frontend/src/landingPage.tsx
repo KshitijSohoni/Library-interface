6ac89cf25de448b3c37b7b331cb4fd1702dbf208
AnshSrivastava2004 <anshsrivastava2004@gmail.com>
2024-06-23 10:41:36 +0530

    Add frontend

---
+++ frontend/src/landingPage.tsx
@@ -0,0 +1,24 @@
+import './landingPage.css'
+
+function LandingPage() {
+
+  return (
+      <div className="main-container">
+        <div className="main-header-wrapper">
+          <div className="main-header">Placeholder</div>
+        </div>
+        <div className="image-button-wrapper">
+          <div className="main-image-container">
+            <img src="./LandingPageImage.png" alt="" />
+          </div>
+          <div className="main-button-container">
+            <button className="main-button">Placeholder</button>
+            <button className="main-button">Placeholder</button>
+            <button className="main-button">Placeholder</button>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default LandingPage;
